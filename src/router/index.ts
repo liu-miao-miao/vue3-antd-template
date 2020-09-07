f597dc30d917faaccf35bfb2e8d941f079ea9c3d
yz_liufawang <446740390@qq.com>
2020-09-07 18:00:39 +0800

    feat：initial project

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,14 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/vue3',
+    name: 'Vue3',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "vue3" */ '../views/Vue3.vue'),
+  },
 ];
 
 const router = createRouter({
